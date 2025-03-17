76222e73ecc3a772824782fbbdd846a14b719a89
AisyahZahradiva <zahradivaaisyah@gmail.com>
2025-03-17 22:51:24 +0700

    Add files via upload

---
+++ Tugas Modul 3.py
@@ -0,0 +1,35 @@
+print('Permainan Tebak Tembakan')
+
+#menentukan batas angka dan angka DOR
+print('Pemain 1')
+batas= int(input('Masukkan Batas Angka Positif: '))
+tembak= int(input('Pilih Angka Kelipatan Tembakan: '))
+i=1
+
+
+#mencetak deretan angka
+while i < batas+1:
+    sisa= i%tembak
+    if sisa == 0:
+        print('DOR', end='  ')
+    else:
+        print(i, end='  ')
+    i=i+1
+
+print('\n')
+
+#menentukan angka untuk menebak
+print('Pemain 2')
+print('Kamu harus menebak angka yang bukan tembakan, Semangatt!!')
+tebak= int(input(f'Masukkan angka yang kamu tebak dari 1-{batas}: '))
+
+
+#mencetak kebenaran dari tebakan pemain 2
+if tebak % tembak == 0 and 1 <= tebak <= batas:
+    print('Yahhh Kamu salah menebak, kalah dehh :(')
+elif 1 <= tebak <= batas :
+    print('Wahhh Selamat kamu menebak dengan benar, Kerenn Kamu Menang ;)') 
+else:
+    print(f'Maaf kamu harus mengulang menebak karena {tebak} berada di luar batasan angka yang tersedia')
+print('\nPermainan Selesai')
+print('Terimakasih telah memainkan permainan ini<3')
